18a66bfb1edbf0bd51dc6d49207ba84eac07bb60
Maijz128 <Maijz128@gmail.com>
2017-05-18 14:11:26 +0800

    实现 浏览首页自动清空消息提醒

---
+++ chrome-extensions/js/content_scripts/home-page.js
@@ -0,0 +1,10 @@
+/**
+ * Auther: MaiJZ
+ * Date: 2017/5/18
+ * Github: https://github.com/maijz128
+ */
+
+console.log('Hello Jandan, from home-page.js');
+
+
+Message.send(MessageName.browse.homeSimplePostList);
